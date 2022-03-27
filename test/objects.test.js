1868330747fa801f1922f5b11fe41ad08e34c297
Ambratolm <Ambratolm@gmail.com>
2022-03-27 03:13:43 +0000

    Refactored test

---
+++ test/objects.test.js
@@ -0,0 +1,106 @@
+///══════════════════════════════════════════════════════════════════════════════
+// ■ Test-Objects (objects.test.js)
+//┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+//     Test objects.
+//══════════════════════════════════════════════════════════════════════════════
+
+//──────────────────────────────────────────────────────────────────────────────
+// ● Rectangle-Class
+//──────────────────────────────────────────────────────────────────────────────
+class Rectangle {
+  _width = 0;
+  _height = 0;
+  _position = { x: 0, y: 0 };
+  // _childs = [ new Rectangle(), new Rectangle(), new Rectangle() ];
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  set width(value) {
+    if (typeof value === "number" && value >= 0) {
+      this._width = value;
+    }
+  }
+  get width() {
+    return this._width;
+  }
+  set height(value) {
+    if (typeof value === "number" && value >= 0) {
+      this._height = value;
+    }
+  }
+  get height() {
+    return this._height;
+  }
+  get area() {
+    return this.width * this.height;
+  }
+  get perimeter() {
+    return 2 * (this.width + this.height);
+  }
+  get diagonal() {
+    return Math.sqrt(Math.pow(this.width, 2) + Math.pow(this.height, 2));
+  }
+  log() {
+    console.log(this);
+  }
+  toString() {
+    return `Rectangle ${this.width}x${this.height} with an area of ${this.area}, a perimeter of ${this.perimeter}, and a diagonal of ${this.diagonal}.`;
+  }
+  [Symbol.toPrimitive](hint) {
+    if (hint === "number") return this.area;
+    if (hint === "string") return `${this.width}x${this.height}`;
+    return `Rectangle`;
+  }
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+// ● Rectangle-Class-Instance
+//──────────────────────────────────────────────────────────────────────────────
+exports.rectInstObj = new Rectangle(128, 32);
+
+//──────────────────────────────────────────────────────────────────────────────
+// ● Rectangle-Litteral-Object
+//──────────────────────────────────────────────────────────────────────────────
+exports.rectLittObj = {
+  _width: 64,
+  _height: 16,
+  _position: { x: 0, y: 0 },
+  _childs: [ new Rectangle(), new Rectangle(), new Rectangle() ],
+  set width(value) {
+    if (typeof value === "number" && value >= 0) {
+      this._width = value;
+    }
+  },
+  get width() {
+    return this._width;
+  },
+  set height(value) {
+    if (typeof value === "number" && value >= 0) {
+      this._height = value;
+    }
+  },
+  get height() {
+    return this._height;
+  },
+  get area() {
+    return this.width * this.height;
+  },
+  get perimeter() {
+    return 2 * (this.width + this.height);
+  },
+  get diagonal() {
+    return Math.sqrt(Math.pow(this.width, 2) + Math.pow(this.height, 2));
+  },
+  log() {
+    console.log(this);
+  },
+  toString() {
+    return `Rectangle ${this.width}x${this.height} with an area of ${this.area}, a perimeter of ${this.perimeter}, and a diagonal of ${this.diagonal}.`;
+  },
+  [Symbol.toPrimitive](hint) {
+    if (hint === "number") return this.area;
+    if (hint === "string") return `${this.width}x${this.height}`;
+    return "Rectangle";
+  }
+}
